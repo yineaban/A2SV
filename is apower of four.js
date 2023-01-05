88236cb0ff6c8ecdef546053b9673077c9f77d9b
yineaban <121093513+yineaban@users.noreply.github.com>
2023-01-05 16:28:49 +0300

    Create is apower of four.js

---
+++ is apower of four.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if(n===0){
+        return false 
+        }
+
+        while(n%4===0){
+            n/=4
+        }
+         return n===1
+    }
+   
