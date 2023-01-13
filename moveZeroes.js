39d5c1c5c75a8dbe4d5b1bc7d4c02fd5d1357843
yineaban <121093513+yineaban@users.noreply.github.com>
2023-01-13 15:41:47 +0300

    Create moveZeroes.js

---
+++ moveZeroes.js
@@ -0,0 +1,15 @@
+function moveZeroes(arr){
+    let nonzero=0
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]!==0){
+            nonzero=arr[i]
+
+            nonzero++
+        }
+        else{
+            arr[i]=0
+
+        }
+    return arr
+}
+}
