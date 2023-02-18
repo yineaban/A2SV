78c10767a3f4c650b5e89f8a16f0f28c918ec6a2
yineaban <121093513+yineaban@users.noreply.github.com>
2023-02-18 15:13:34 +0300

    Create merge intervals.js

---
+++ merge intervals.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    intervals.sort((a,b)=>a[0]-b[0])
+    for(let i =1; i<intervals.length;i++){
+        let [x,y] = intervals[i]
+        let [prevX,prevY] = intervals[i-1]
+        if(x <= prevY) {
+            intervals[i-1][1] = Math.max(y,prevY);
+            intervals.splice(i,1)
+            i--
+        }
+    }
+    return intervals    
+};
